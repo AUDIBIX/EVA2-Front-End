976d8959a5ad0b9ce3e2a9234f9095c378f20feb
AUDIBIX <cbbenja@hotmail.com>
2025-05-26 19:03:31 -0400

    Correcion

--- script2.js
+++ script2.js
@@ -22,10 +22,32 @@ function validar() {
 }
 
 function validarNombre(elemento,valor,error) {
+    if (valor.length < 1) {
+        elemento.style.backgroundColor = 'red'
+        error.innerText = 'El campo debe contener al menos 1 caracter'
+        return false
+    }
+
+    for (let caracter of valor) {
+        if (!(/^[a-zA-Z]+$/.test(caracter))) {
+            elemento.style.backgroundColor = 'red'
+            error.innerText = 'El campo no debe contener caracteres especiales ni espacios'
+            return false
+        }
+    }
+    elemento.style.backgroundColor = 'green'
+    error.innerText = ''
     return true
 }
 
 function validarEdad(elemento,valor,error) {
+    if (valor<18||valor>99) {
+        elemento.style.backgroundColor = 'red'
+        error.innerText = 'La edad debe estar entre 18 y 99 años inclusive'
+        return false
+    }
+    elemento.style.backgroundColor = 'green'
+    error.innerText = ''
     return true
 }
 
@@ -44,9 +66,38 @@ function cargarTabla() {
 }
 
 function eliminar(indice) {
-    let p = personas.filter((persona, index)=>{
+    personas = personas.filter((p, index)=> {
+        if (indice == index) {
+            return false
+        } else {return true}
+    })
+    cargarTabla()
+}
+
+function actualizarFormulario(indice) {
+    document.getElementById('nombre').value = personas[indice].nombre
+    document.getElementById('edad').value = personas[indice].edad
+    document.getElementById('btnActualizar').value = indice
+}
+
+function actualizar() {
+    let eNombre = document.getElementById('nombre')
+    let eEdad = document.getElementById('edad')
+    let eBoton = document.getElementById('btnActualizar')
+    let vNombre = eNombre.value
+    let vEdad = eEdad.value
+    let indice = eBoton.value
+    let errorNombre = document.getElementById('errorNombre')
+    let errorEdad = document.getElementById('errorEdad')
+    let fNombre = validarNombre(eNombre,vNombre,errorNombre)
+    let fEdad = validarEdad(eEdad,vEdad,errorEdad)
+    if (!fNombre || !fEdad) {return false}
+    personas = personas.map((persona,index) => {
         if (index == indice) {
-            return 
+            return {
+                nombre : vNombre,
+                edad : vEdad
+            }
         } else return persona
     })
     cargarTabla()
